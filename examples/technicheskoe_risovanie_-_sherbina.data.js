474038ab0009283c91c0288a083443088a07c11d
sushilshik <ahundov@gmail.com>
2019-10-20 22:16:24 +0300

    zoom fix. Examples added: book scheme and vis.js own js code scheme

---
+++ examples/technicheskoe_risovanie_-_sherbina.data.js
@@ -0,0 +1,124 @@
+var schemeData = 
+{
+	"canvas1Data": {
+		"nodes": {
+			"_subscribers": {
+				"*": [],
+				"add": [
+				{}
+				],
+				"remove": [
+				{}
+				],
+				"update": [
+				{}
+				]
+			},
+			"_options": {},
+			"_data": {
+				"41b08024-14c3-4317-8e50-dbbb2a2349cb": {
+					"id": "41b08024-14c3-4317-8e50-dbbb2a2349cb",
+					"x": 2154,
+					"y": 152,
+					"label": "                                    ",
+					"shape": "box",
+					"link": "",
+					"color": {
+						"background": "rgba(0,0,0,0)",
+						"border": "red"
+					},
+					"borderWidth": "3"
+				},
+				"2414ec88-8aea-4ed0-b565-e37796e8e221": {
+					"id": "2414ec88-8aea-4ed0-b565-e37796e8e221",
+					"x": 2401,
+					"y": 151,
+					"label": "Теория аксонометрических проекций"
+				}
+			},
+			"length": 0,
+			"_idProp": "id",
+			"_type": {}
+		},
+		"edges": {
+			"_subscribers": {
+				"*": [],
+				"add": [
+				{}
+				],
+				"remove": [
+				{}
+				],
+				"update": [
+				{}
+				]
+			},
+			"_options": {},
+			"_data": {
+				"f5a0f766-4c2a-4297-b0c0-d79eb4c96f62": {
+					"from": "2414ec88-8aea-4ed0-b565-e37796e8e221",
+					"to": "41b08024-14c3-4317-8e50-dbbb2a2349cb",
+					"id": "f5a0f766-4c2a-4297-b0c0-d79eb4c96f62"
+				}
+			},
+			"length": 0,
+			"_idProp": "id",
+			"_type": {}
+		}
+	},
+	"canvas2Data": {
+		"nodes": {
+			"_subscribers": {
+				"*": [],
+				"add": [
+				{},
+				{}
+				],
+				"remove": [
+				{},
+				{}
+				],
+				"update": [
+				{},
+				{}
+				]
+			},
+			"_options": {},
+			"_data": {
+			},
+			"length": 0,
+			"_idProp": "id",
+			"_type": {}
+		},
+		"edges": {
+			"_subscribers": {
+				"*": [],
+				"add": [
+				{},
+				{}
+				],
+				"remove": [
+				{},
+				{}
+				],
+				"update": [
+				{},
+				{}
+				]
+			},
+			"_options": {},
+			"_data": {},
+			"length": 0,
+			"_idProp": "id",
+			"_type": {}
+		}
+	},
+	"setup": {
+		"scale": 0.135,
+		"viewPosition": {
+			"x": 2896.4214,
+			"y": 637.1176
+		}
+	}
+}
+;
---
+++ examples/vis.js.data.js
@@ -0,0 +1,100 @@
+var schemeData = 
+{
+	"canvas1Data": {
+		"nodes": {
+			"_subscribers": {
+				"*": [],
+				"add": [
+				{}
+				],
+				"remove": [
+				{}
+				],
+				"update": [
+				{}
+				]
+			},
+			"_options": {},
+			"_data": {
+			},
+			"length": 0,
+			"_idProp": "id",
+			"_type": {}
+		},
+		"edges": {
+			"_subscribers": {
+				"*": [],
+				"add": [
+				{}
+				],
+				"remove": [
+				{}
+				],
+				"update": [
+				{}
+				]
+			},
+			"_options": {},
+			"_data": {
+			},
+			"length": 0,
+			"_idProp": "id",
+			"_type": {}
+		}
+	},
+	"canvas2Data": {
+		"nodes": {
+			"_subscribers": {
+				"*": [],
+				"add": [
+				{},
+				{}
+				],
+				"remove": [
+				{},
+				{}
+				],
+				"update": [
+				{},
+				{}
+				]
+			},
+			"_options": {},
+			"_data": {
+			},
+			"length": 0,
+			"_idProp": "id",
+			"_type": {}
+		},
+		"edges": {
+			"_subscribers": {
+				"*": [],
+				"add": [
+				{},
+				{}
+				],
+				"remove": [
+				{},
+				{}
+				],
+				"update": [
+				{},
+				{}
+				]
+			},
+			"_options": {},
+			"_data": {},
+			"length": 0,
+			"_idProp": "id",
+			"_type": {}
+		}
+	},
+	"setup": {
+		"scale": 0.046,
+		"viewPosition": {
+			"x": -313.4214,
+			"y": 5738.1176
+		}
+	}
+}
+;
